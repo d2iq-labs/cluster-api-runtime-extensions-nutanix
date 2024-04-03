06d676a114a08014085ccb8fd884679b6001dd2f
Shalin Patel <shalin.patel@nutanix.com>
2024-04-03 12:59:39 -0700

    test: move aws network tests to its own package

--- pkg/handlers/aws/mutation/network/inject_test.go
+++ pkg/handlers/aws/mutation/network/inject_test.go
@@ -1,45 +1,49 @@
 // Copyright 2023 D2iQ, Inc. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-package tests
+package network
 
 import (
 	"testing"
 
+	. "github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 	runtimehooksv1 "sigs.k8s.io/cluster-api/exp/runtime/hooks/api/v1alpha1"
 
 	"github.com/d2iq-labs/cluster-api-runtime-extensions-nutanix/api/v1alpha1"
 	"github.com/d2iq-labs/cluster-api-runtime-extensions-nutanix/common/pkg/capi/clustertopology/handlers/mutation"
 	"github.com/d2iq-labs/cluster-api-runtime-extensions-nutanix/common/pkg/testutils/capitest"
 	"github.com/d2iq-labs/cluster-api-runtime-extensions-nutanix/common/pkg/testutils/capitest/request"
+	"github.com/d2iq-labs/cluster-api-runtime-extensions-nutanix/pkg/handlers/generic/clusterconfig"
 )
 
-func TestGeneratePatches(
-	t *testing.T,
-	generatorFunc func() mutation.GeneratePatches,
-	variableName string,
-	variablePath ...string,
-) {
-	t.Helper()
+func TestNetworkPatch(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "AWS Network mutator suite")
+}
 
-	capitest.ValidateGeneratePatches(
-		t,
-		generatorFunc,
-		capitest.PatchTestDef{
+var _ = Describe("Generate AWS Network patches", func() {
+	patchGenerator := func() mutation.GeneratePatches {
+		return mutation.NewMetaGeneratePatchesHandler("", NewPatch()).(mutation.GeneratePatches)
+	}
+
+	testDefs := []capitest.PatchTestDef{
+		{
 			Name: "unset variable",
 		},
-		capitest.PatchTestDef{
+		{
 			Name: "VPC ID set",
 			Vars: []runtimehooksv1.Variable{
 				capitest.VariableWithValue(
-					variableName,
+					clusterconfig.MetaVariableName,
 					v1alpha1.AWSNetwork{
 						VPC: &v1alpha1.VPC{
 							ID: "vpc-1234",
 						},
 					},
-					variablePath...,
+					v1alpha1.AWSVariableName,
+					VariableName,
 				),
 			},
 			RequestItem: request.NewAWSClusterTemplateRequestItem("1234"),
@@ -49,11 +53,11 @@ func TestGeneratePatches(
 				ValueMatcher: gomega.Equal("vpc-1234"),
 			}},
 		},
-		capitest.PatchTestDef{
+		{
 			Name: "Subnet IDs set",
 			Vars: []runtimehooksv1.Variable{
 				capitest.VariableWithValue(
-					variableName,
+					clusterconfig.MetaVariableName,
 					v1alpha1.AWSNetwork{
 						Subnets: v1alpha1.Subnets{
 							{ID: "subnet-1"},
@@ -61,7 +65,8 @@ func TestGeneratePatches(
 							{ID: "subnet-3"},
 						},
 					},
-					variablePath...,
+					v1alpha1.AWSVariableName,
+					VariableName,
 				),
 			},
 			RequestItem: request.NewAWSClusterTemplateRequestItem("1234"),
@@ -71,11 +76,11 @@ func TestGeneratePatches(
 				ValueMatcher: gomega.HaveLen(3),
 			}},
 		},
-		capitest.PatchTestDef{
+		{
 			Name: "both VPC ID and Subnet IDs set",
 			Vars: []runtimehooksv1.Variable{
 				capitest.VariableWithValue(
-					variableName,
+					clusterconfig.MetaVariableName,
 					v1alpha1.AWSNetwork{
 						VPC: &v1alpha1.VPC{
 							ID: "vpc-1234",
@@ -86,7 +91,8 @@ func TestGeneratePatches(
 							{ID: "subnet-3"},
 						},
 					},
-					variablePath...,
+					v1alpha1.AWSVariableName,
+					VariableName,
 				),
 			},
 			RequestItem: request.NewAWSClusterTemplateRequestItem("1234"),
@@ -100,5 +106,17 @@ func TestGeneratePatches(
 				ValueMatcher: gomega.HaveLen(3),
 			}},
 		},
-	)
-}
+	}
+
+	// create test node for each case
+	for testIdx := range testDefs {
+		tt := testDefs[testIdx]
+		It(tt.Name, func() {
+			capitest.AssertGeneratePatches(
+				GinkgoT(),
+				patchGenerator,
+				&tt,
+			)
+		})
+	}
+})
