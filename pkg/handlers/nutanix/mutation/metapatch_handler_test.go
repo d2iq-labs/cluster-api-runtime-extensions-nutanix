2a8b4e30e8cbd5fc972a2f13dc10267cd6de7c99
Shalin Patel <shalin.patel@nutanix.com>
2024-04-04 20:58:16 -0700

    test: remove gereric unit tests from nutanix meta patch handler

---
+++ pkg/handlers/nutanix/mutation/metapatch_handler_test.go
@@ -0,0 +1,31 @@
+// Copyright 2024 D2iQ, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package mutation
+
+import (
+	"testing"
+
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+
+	"github.com/d2iq-labs/cluster-api-runtime-extensions-nutanix/common/pkg/capi/clustertopology/handlers/mutation"
+)
+
+func metaPatchGeneratorFunc(mgr manager.Manager) func() mutation.GeneratePatches {
+	return func() mutation.GeneratePatches {
+		return MetaPatchHandler(mgr).(mutation.GeneratePatches)
+	}
+}
+
+func workerPatchGeneratorFunc() func() mutation.GeneratePatches {
+	return func() mutation.GeneratePatches {
+		return MetaWorkerPatchHandler().(mutation.GeneratePatches)
+	}
+}
+
+func TestGeneratePatches(t *testing.T) {
+	t.Parallel()
+
+	mgr := testEnv.Manager
+
+}
